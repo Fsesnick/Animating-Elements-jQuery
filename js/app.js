1ebe151acf974440df1ca0438485846cd9c7df3d
Fsesnick <fernandasesnick@gmail.com>
2021-02-01 14:28:21 +0000

    Changing content inside elements

--- js/app.js
+++ js/app.js
@@ -8,8 +8,14 @@ $('#flashMessage').slideUp();
 
 //$('#flashMessage').hide().slideDown(1000).delay(3000).slideUp();
 
-('#flashMessage')
+$('#flashMessage')
   .hide()
   .slideDown(1000)
   .delay(3000)
   .slideUp();
+
+const title = " Minha primeira postagem do blog. ";
+const content = "Este é meu primeiro post'";
+
+$('#blogTitlePreview').text(title);
+$('#blogContentPreview').text(content);
